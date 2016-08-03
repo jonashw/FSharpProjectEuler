ed43beff1e76d60d112f629d47b6105aa67ee666
Jon Wilson <2jonwilson@gmail.com>
2016-08-02 17:33:52 -0700

    Initial commit. Add solutions for problems 1-3, 54

---
+++ ProjectEulerFSharp/Problem001.fs
@@ -0,0 +1,10 @@
+﻿module Problem001
+(*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+*)
+let isMultiple x = (x % 3 = 0) || (x % 5 = 0)
+let multipleSum n = [1..n] 
+                    |> Seq.filter isMultiple
+                    |> Seq.sum
+let solution = multipleSum (1000-1)
---
+++ ProjectEulerFSharp/Problem003.fs
@@ -0,0 +1,24 @@
+﻿module Problem003
+
+(*  LARGEST PRIME FACTOR 
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143?
+*)
+let sievePrimesUpTo n = 
+    let sieve = new System.Collections.Generic.Dictionary<int64,bool>()
+    [1L..n] |> List.iter (fun x -> sieve.[x] <- true)
+    let pMax = n |> (float) |> sqrt |> (int64)
+    let ps = [2L..pMax]
+    ps |> Seq.filter (fun p -> sieve.[p])
+       |> Seq.collect (fun p -> [p*p..p..n])
+       |> Seq.iter (fun x -> sieve.[x] <- false)
+    sieve |> Seq.filter (fun pair -> pair.Value)
+          |> Seq.map (fun pair -> pair.Key)
+          |> List.ofSeq
+
+let isFactor n f = (((float) n) / ((float) f)) |> (fun x -> x - floor x) = 0.0
+
+let primeFactors n = sievePrimesUpTo n |> List.filter (isFactor n)
+    
+let solution = primeFactors 600851475143L
+/// Problem: OutOfMemoryException
